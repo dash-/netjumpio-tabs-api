0ff14481207b4ef6d0bb6e8aac4fecf3f481a45a
Basic <basic@localhost>
2016-12-01 16:20:30 -0700

    Added additional env vars (admin name, user, email)

--- env-example.sh
+++ env-example.sh
@@ -4,4 +4,7 @@
 # ~/.nj-tabsetsrc, and then customized.  The file can then be imported via
 # the `source` command prior to running the API daemon.
 
+export NETJUMP_IO__TABSETS__ADMIN_NAME="Admin"
+export NETJUMP_IO__TABSETS__ADMIN_USER="admin"
+export NETJUMP_IO__TABSETS__ADMIN_EMAIL="admin@tabsets.com"
 export NETJUMP_IO__TABSETS__ADMIN_PASS="Some Password"
