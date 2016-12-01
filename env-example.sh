337569d70186e91058b48d2eebeadee8e0d29605
Basic <basic@localhost>
2016-12-01 16:18:19 -0700

    Added example environment config script

---
+++ env-example.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# This file should be copied outside of this repository, typically to
+# ~/.nj-tabsetsrc, and then customized.  The file can then be imported via
+# the `source` command prior to running the API daemon.
+
+export NETJUMP_IO__TABSETS__ADMIN_PASS="Some Password"
