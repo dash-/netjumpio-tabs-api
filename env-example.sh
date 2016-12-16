d370e47a284875a1dc23cba4626e87617964004e
David Ash <green.neon@gmail.com>
2016-12-16 16:05:28 -0700

    Updating documentation

--- env-example.sh
+++ env-example.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # This file should be copied outside of this repository, typically to
-# ~/.nj-tabsetsrc, and then customized.  The file can then be imported via
+# ~/.tabs-api-env.sh, and then customized.  The file can then be imported via
 # the `source` command prior to running the API daemon.
 
 export NETJUMP_IO__TABSETS__ADMIN_NAME="Admin"
