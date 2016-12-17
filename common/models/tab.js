9c210499dfeba559f4136d3509d27aee08d751d1
David Ash <green.neon@gmail.com>
2016-12-17 16:32:18 -0700

    feat/cr36 - Added mocked-up webpage retrieval api

--- common/models/tab.js
+++ common/models/tab.js
@@ -1,5 +1,19 @@
 'use strict';
 
 module.exports = function(Tab) {
+	Tab.webpage = function(url) {
+		return new Promise((resolve, reject) => {
+			return resolve({
+				logoUrl: 'https://lh3.googleusercontent.com/-qn7Hl_AXEOA/Vegu58jvBrI/AAAAAAAAAVA/RtkSABWWZsU/w768-h768/Google_icon_2015.png',
+				name: 'Google',
+				url: url,
+			});
+		});
+	};
 
+	Tab.remoteMethod('webpage', {
+		http: {path: '/webpage', verb: 'get'},
+		accepts: {arg: 'url', type: 'string'},
+		returns: {type: 'object', root: true},
+	});
 };
