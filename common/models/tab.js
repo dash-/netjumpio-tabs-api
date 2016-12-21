6824a2bc4a1016db820373153de7e86283ccc962
Jon Barrett <jonab1974@gmail.com>
2016-12-21 21:02:21 +0000

    feat/cr8 - Replaced mock up with working api

--- common/models/tab.js
+++ common/models/tab.js
@@ -1,13 +1,42 @@
 'use strict';
 
+
+///
+// Dependencies
+///
+
+var path = require('path');
+var urlParser = require('url');
+var rp = require('request-promise');
+var cheerio = require('cheerio');
+
+
+///
+// Configuration
+///
+
+const config = {
+	userAgent: (
+		'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 ' +
+		'(KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36'
+	),
+}
+
+
+///
+// Module
+///
+
 module.exports = function(Tab) {
 	Tab.webpage = function(url) {
-		return new Promise((resolve, reject) => {
-			return resolve({
-				logoUrl: 'https://lh3.googleusercontent.com/-qn7Hl_AXEOA/Vegu58jvBrI/AAAAAAAAAVA/RtkSABWWZsU/w768-h768/Google_icon_2015.png',
-				name: 'Google',
+		return rp(buildRPOptions(url)).then((data) => {
+			return processPage(url, data);
+		}).catch((err) => {
+			return {
+				logoUrl: '',
+				name: '',
 				url: url,
-			});
+			};
 		});
 	};
 
@@ -17,3 +46,86 @@ module.exports = function(Tab) {
 		returns: {type: 'object', root: true},
 	});
 };
+
+
+///
+// Helpers
+///
+
+function buildRPOptions(url) {
+	return {
+		uri: url,
+		headers: {
+			'Upgrade-Insecure-Requests': 1,
+			'User-Agent': config.userAgent,
+		},
+		json: true,
+		gzip: true
+	};
+}
+
+function processPage(url, data) {
+	const $ = cheerio.load(data);
+
+	return {
+		logoUrl: getLogoUrl(url, $),
+		name: getTitle($),
+		url: url,
+	}
+}
+
+function getLogoUrl(url, $) {
+	let logoUrl = '';
+
+	$('link').each((tagIdx, tag) => {
+		const rel = $(tag).attr('rel') || '';
+		if(rel.toLowerCase() !== 'shortcut icon') {
+			// .each continue
+			return;
+		}
+
+		const href = $(tag).attr('href');
+
+		if(! href) {
+			// .each continue
+			return;
+		}
+
+		logoUrl = toAbsoluteUrl(url, href);
+
+		return false;
+	});
+
+	return logoUrl;
+}
+
+function getTitle($) {
+	return $('title').text();
+}
+
+function toAbsoluteUrl(baseUrl, relativeUrl) {
+	const parsedBaseUrl = urlParser.parse(baseUrl);
+
+	if(relativeUrl.match(/^\/\//)) {
+		relativeUrl = parsedBaseUrl.protocol + relativeUrl;
+	}
+
+	let parsedRelativeUrl = urlParser.parse(relativeUrl);
+
+	if(! parsedRelativeUrl.protocol) {
+		parsedRelativeUrl.protocol = parsedBaseUrl.protocol;
+	}
+
+	if(! parsedRelativeUrl.host) {
+		parsedRelativeUrl.host = parsedBaseUrl.host;
+	}
+
+	if(! path.isAbsolute(parsedRelativeUrl.path)) {
+		parsedRelativeUrl.path = path.resolve(
+			parsedBaseUrl.path, 
+			parsedRelativeUrl.path
+		);
+	}
+
+	return urlParser.format(parsedRelativeUrl);
+}
